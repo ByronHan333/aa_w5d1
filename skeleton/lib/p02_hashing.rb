47b074fcc4d3188e9610c8fc6693f9d8bb07843a
AA Student <appacademy@Mini-15.local>
2022-10-31 14:41:35 -0700

    string, arr, hash hashing functions

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    hash_value = 0
+    self.each_with_index do |v , i|
+      hash_value += (v + i ** 2).to_s.ord
+    end
+    hash_value
   end
 end
 
 class String
   def hash
+    hash_value = 0
+    self.each_char.with_index do |v , i|
+      hash_value += (v.to_s.ord * i.to_s.ord ** 2)
+    end
+    hash_value
   end
 end
 
@@ -16,6 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_value = 0
+    self.each_pair do |v , i|
+      hash_value += (v.to_s.ord * i.to_s.ord)
+    end
+    hash_value
   end
 end
