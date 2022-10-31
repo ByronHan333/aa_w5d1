f98d8793b3eff5cc7d5bbcc79e4fb3db65a458ee
AA Student <appacademy@Mini-15.local>
2022-10-31 14:22:39 -0700

    p01

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -35,11 +35,13 @@ class IntSet
   end
 
   def insert(num)
-    idx = num % 20
-    @store[idx] << num
+    bucket = self[num]
+    bucket << num
   end
 
   def remove(num)
+    bucket = self[num]
+    bucket.delete(num)
   end
 
   def include?(num)
@@ -73,24 +75,67 @@ class ResizingIntSet
   end
 
   def insert(num)
+
+    resize! if @count == num_buckets
+    bucket = self[num]
+    i = 0
+    while i < bucket.length
+      return if bucket[i] == num
+      i+=1
+    end
+    bucket << num 
+    @count += 1
   end
 
   def remove(num)
+    bucket = self[num]
+    if bucket.delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
-  end
-
-  private
-
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    bucket = self[num]
+    i = 0
+    while i < bucket.length
+      return true if bucket[i] == num
+      i+=1
+    end
+    false
   end
 
   def num_buckets
     @store.length
   end
 
+  private
+
+  attr_accessor :store
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    return @store[num % num_buckets]
+  end
+
+  # def num_buckets
+  #   @store.length
+  # end
+
   def resize!
+    if num_buckets == count
+      new_store = Array.new(num_buckets * 2) { Array.new }
+      @store.each do |bucket|
+        bucket.each do |ele|
+          new_store[ele % (num_buckets * 2)] << ele
+        end
+      end
+      self.store = new_store
+    end
   end
 end
+
+set = ResizingIntSet.new 
+p set.num_buckets
+puts 
+21.times { |i| set.insert(i) }
+p set.num_buckets
